dc80b988fee1b05868aca773b1c0c1bbb4df81a9
Ernesto Hernandez <h_ernesto@ymail.com>
2018-03-05 21:15:24 -0800

    changed to index

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+/** Create the express app */
+const app = express();
+
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+
+/** Declare usage of express app */
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.engine("handlebars", handlebars({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+require("./routes/apiRoutes");
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, function(){
+    console.log("Listening on port: " + PORT);
+});
